edac6d4f894bfde088a7a25d217c86b73e4fad3d
Mohit Sankhla <82494250+Mohit-sankhla02@users.noreply.github.com>
2023-05-13 19:55:33 +0530

    added router and layout (singup and login pages)

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
-import Nav from "./components/Nav";
-
+import {Routes, Route} from 'react-router-dom'
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/loginPage";
+import SignupPage from "./pages/SignupPage";
+import Layout from "./Layout";
 function App() {
-  return (
-    <React.Fragment>
-      <Nav />
-    </React.Fragment>
-  );
+    return (
+        <Routes>
+            <Route element={<Layout />}>
+                <Route index element={<IndexPage />} />
+                <Route path={`/login`} element={<LoginPage />} />
+                <Route path={`/signup`} element={<SignupPage /> } />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        fontY: '"Ysabeau", sans-serif'
+      },
       colors: {
         'primary': '#fc037f',
         'primaryhover': '#'
